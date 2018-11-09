02017827ebd69934f920108c7f604d55bcf3d10a
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-11-09 15:39:14 +0100

    Add CARVING_MODEL state to the ClientState

--- client/src/main/java/org/evosuite/rmi/service/ClientState.java
+++ client/src/main/java/org/evosuite/rmi/service/ClientState.java
@@ -23,7 +23,7 @@ import org.evosuite.Properties;
 
 /**
  * FIXME: sync with ProgressMonitor, and finish set it in all client code
- * 
+ *
  * @author arcuri
  *
  */
@@ -35,71 +35,72 @@ public enum ClientState {
 	STARTED("Started", "Client process has been launched", 1),
 	INITIALIZATION("Initializing", "Analyzing classpath and dependencies", 2),
 	CARVING("Carving", "Carving JUnit tests", 3),
-	SEARCH("Search", "Generating test cases", 4),
-	INLINING("Inlining", "Inlining constants", 5),
-	MINIMIZING_VALUES("Minimizing values", "Minimizing primitive values in the tests", 6),
-	MINIMIZATION("Minimizing", "Minimizing size/length of test cases", 7),
+	CARVING_MODEL("Carving", "Carving behavioral models", 4),
+	SEARCH("Search", "Generating test cases", 5),
+	INLINING("Inlining", "Inlining constants", 6),
+	MINIMIZING_VALUES("Minimizing values", "Minimizing primitive values in the tests", 7),
+	MINIMIZATION("Minimizing", "Minimizing size/length of test cases", 8),
 	//TODO, question: why is it before ASSERTION?
-	COVERAGE_ANALYSIS("Coverage Analysis","Compute and the different coverage criteria of the generated test suite",8),
-	ASSERTION_GENERATION("Generating assertions", "Adding assertions to the test cases", 9),
-	JUNIT_CHECK("Check JUnit", "Validate and fix the generated tests",10),
-	WRITING_TESTS("JUnit", "Writing JUnit tests to disk", 11),
-    WRITING_STATISTICS("Statistics", "Writing statistics to disk", 12),
-	DONE("Done", "Test case generation is finished", 13),
-	FINISHED("Finished", "Client process is fully finished", 14);
+	COVERAGE_ANALYSIS("Coverage Analysis","Compute and the different coverage criteria of the generated test suite",9),
+	ASSERTION_GENERATION("Generating assertions", "Adding assertions to the test cases", 10),
+	JUNIT_CHECK("Check JUnit", "Validate and fix the generated tests",11),
+	WRITING_TESTS("JUnit", "Writing JUnit tests to disk", 12),
+    WRITING_STATISTICS("Statistics", "Writing statistics to disk", 13),
+	DONE("Done", "Test case generation is finished", 14),
+	FINISHED("Finished", "Client process is fully finished", 15);
 
 	private String name;
 	private String description;
 	private int numPhase = 0;
 	private int progress = 0;
-	
+
 	private int startProgress = 0;
 	private int maxProgress = 0;
-	
+
 	ClientState(String name, String description, int numPhase) {
 		this.name = name;
 		this.description = description;
 		this.numPhase = numPhase;
 		setProgressBoundaries(numPhase);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public int getNumPhase() {
 		return numPhase;
 	}
-	
+
 	public int getOverallProgress() {
 		return progress;
 	}
-	
+
 	public int getStartProgress() {
 		return startProgress;
 	}
-	
+
 	public int getEndProgress() {
 		return maxProgress;
 	}
-	
+
 	public int getPhaseProgress() {
 		int divisor = maxProgress - startProgress;
 		if(divisor == 0)
 			return 0;
-		return (progress - startProgress) / divisor; 
+		return (progress - startProgress) / divisor;
 	}
-	
+
 	public void setProgressOfPhase(float progress) {
 		this.progress = startProgress + (int)((maxProgress - startProgress) * progress);
 	}
-	
-	
-	
+
+
+
 	private void setProgressBoundaries(int numPhase) {
 
 		switch(numPhase) {
@@ -108,13 +109,13 @@ public enum ClientState {
 			startProgress = 1;
 			maxProgress = 1;
 			break;
-			
+
 		case 2: // Static analysis started
 			progress = 2;
 			startProgress = 2;
 			maxProgress = 9;
 			break;
-			
+
 		case 3: // Test carving - TODO
 			progress = 5;
 			startProgress = 5;
@@ -135,7 +136,7 @@ public enum ClientState {
 			if(Properties.ASSERTIONS) {
 				startProgress = 33;
 			} else {
-				startProgress = 66;				
+				startProgress = 66;
 			}
 			maxProgress = startProgress + 5;
 			progress = startProgress;
@@ -149,7 +150,7 @@ public enum ClientState {
 			maxProgress = startProgress + 5;
 			progress = startProgress;
 			break;
-			
+
 		case 7: // minimizing tests
 			if(Properties.ASSERTIONS) {
 				startProgress = Properties.INLINE ? 33 + 5 : 33;
@@ -181,21 +182,21 @@ public enum ClientState {
 			maxProgress = 94;
 			progress = startProgress;
 			break;
-			
+
 		case 11: // writing tests
-			startProgress = 95;				
+			startProgress = 95;
 			maxProgress = 98;
 			progress = startProgress;
 			break;
-			
+
 		case 12: // shutting down
-			startProgress = 99;				
+			startProgress = 99;
 			maxProgress = 100;
 			progress = startProgress;
 			break;
-						
+
 		default:
-			startProgress = 100;				
+			startProgress = 100;
 			maxProgress = 100;
 			progress = 100;
 		}
